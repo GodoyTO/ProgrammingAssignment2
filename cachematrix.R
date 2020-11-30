2d8724b7a4d9126fa5abca81e78eb713130bb1db
GodoyTO <tgodoyoliveira@gmail.com>
2020-11-30 17:21:38 -0300

    First real try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Just a simple function capable to save a function in the cache, so you can 
+## latter see if it's has an inverse matrix. It does it by firstly check if the
+## determinant exist, and if so, applying the inverse function to the first matrix
 
-## Write a short comment describing this function
+install.packages("matlib")
+library(matlib)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) im <<- inv
+        getinv <- function() im
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Here we check if the determinant is real. If so, apply the inverse function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinv()
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- inv(data, ...)
+        x$setinv(im)
+        im
 }
+
+## A simple test to see if both functions works
+
+test<-makeCacheMatrix(matrix(1:4,2,2))
+I<-cacheSolve(test)
+a<-matrix(1:4,2,2)
+a%*%I
+
+##looks like they work fine
